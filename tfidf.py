5158f6dca30b76cbe6833428ba8d9d9f0999dc1b
elshire7311 <elshire@uw.edu>
2025-05-03 10:24:35 -0700

    python files for cleaning and tfidf

--- tfidf.py
+++ tfidf.py
@@ -1,14 +1,53 @@
+import argparse
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+nltk.download('wordnet')
+from nltk.corpus import wordnet
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import regexp_tokenize, word_tokenize, RegexpTokenizer
+import pandas as pd
+import re
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-# Make the TF-IDF vectorizer
-vectorizer = TfidfVectorizer(analyzer = 'word', ngram_range=(1,1))
+def get_wordnet_pos(treebank_tag):
+    '''
+    Translate nltk POS to wordnet tags
+    '''
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
 
-# Get the clean column from the elife dataframe
-elife_train_clean = df_elife_train_clean.clean.tolist()
 
-# Calculate the TF-IDF score for unigrams and bigrams using the clean data
-elife_t_c = vectorizer.fit_transform(elife_train_clean)
+def data_cleaner(doc):
+    """A function to strip punctuation, strip stopwords, casefold, lemmatize,
+    And part of speech tag words for clean data for modeling"""
+    custom_stops = ['doi', 'figure', 'elife', 'et', 'al']
+    sw = stopwords.words('english')
+    regex_token = RegexpTokenizer(r"([a-zA-Z]+(?:’[a-z]+)?)")
+    doc = regex_token.tokenize(doc)
+    doc = [word.lower() for word in doc]
+    doc = [word for word in doc if word not in sw and word not in custom_stops]
+    doc = pos_tag(doc)
+    doc = [(word[0], get_wordnet_pos(word[1])) for word in doc]
+    lemmatizer = WordNetLemmatizer() 
+    doc = [lemmatizer.lemmatize(word[0], word[1]) for word in doc]
+    return ' '.join(doc)
+
+#initally removes things between parenthesis to keep sentence number stable in later process
+def remove_between_parens(doc):
+    doc = re.sub(r"\([^()]*\)|\[[^\]]*\]|\{[^}]*\}", "", doc)
+    return doc
 
 def sentence_value_creator_2(doc_sents: list, vect_obj) -> list: # List of sentences in a document -> list of tuples (index, sum)
     """ 
@@ -27,37 +66,72 @@ def sentence_value_creator_2(doc_sents: list, vect_obj) -> list: # List of sente
         sent_index_val_dict.append((i,float(c))) # Append a tuple of (index, score) for the sentence 
     return sent_index_val_dict # Return the list of sentence (index, score) tuples
 
-summary_percent = 0.4 # Percentage of total document sentences to save as the summary
-doc_summaries = [] # List to store the document summaries
-doc_list = df_elife_train_clean.article.tolist() # List of all the docs in elife train
+if __name__ == "__main__":
+    # argparse logic
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--summary_percent", type=float, default=0.4) # This is the percent of the document that calculates the summary length
+    parser.add_argument(
+        "--clean_csv", # This is the path to the csv with a clean column 
+        type=str,
+        default="data/df_elife_train_clean.csv",
+    )
+    parser.add_argument(
+        "--output_csv", # This is where the new dataframe with a summaries column will be saved
+        type=str,
+        dealault="data/elife_summaries.csv"
+    )
+    parser.add_argument(
+        "--output_txt", #This is where the summaries will be saved to as a txt document
+        type=str,
+        default="data/elife_summaries.txt"
+    )
+    args = parser.parse_args()
+
+    df_clean = pd.read_csv(args.clean_csv) # Read the clean csv in as a pandas dataframe
+
+    # Make the TF-IDF vectorizer
+    vectorizer = TfidfVectorizer(analyzer = 'word', ngram_range=(1,1))
+
+    # Get the clean column from the dataframe
+    clean_column = df_clean.clean.tolist()
+
+    # Calculate the TF-IDF score for unigrams and bigrams using the clean data
+    vector_scores = vectorizer.fit_transform(clean_column)
 
 
-for doc in doc_list: # For every document
-    no_parens = remove_between_parens(doc) # Remove citations and other parentheses from the document
-    doc_sents = nltk.sent_tokenize(no_parens) # Split the document into sentences
-    doc_length = len(doc_sents) # Get the total number of sentences in the document
-    top_num = int(summary_percent * doc_length) # Calculate the number of sentences to keep for the summary
-    sent_scores = sentence_value_creator_2(doc_sents, elife_t_c) # Get list of (index, score) pairs for all the document sentences
-    sorted_scores = sorted(sent_scores, key=lambda x: x[1]) # Sort based on second tuple object; sort by score
-    sorted_scores = sorted_scores[-top_num:] # Crop to just the top top_num sents
-    sorted_sents = sorted(sorted_scores, key = lambda x: x[0]) # Sort the top sents by index so they are in the logical order
-    doc_summary = "" # Save summary of the document as string
-    for (index, score) in sorted_sents: # For every (index, score) pair
-        sent = doc_sents[index] # Get the original sentence using the index
-        doc_summary = doc_summary + sent + " " # Add the original sent to the full summary 
-    doc_summaries.append(doc_summary) # Add the summary to the list of all summaries
+    summary_percent = 0.4 # Percentage of total document sentences to save as the summary
+    doc_summaries = [] # List to store the document summaries
+    doc_list = df_clean.article.tolist() # List of all the docs in the article column
 
-# Save the summaries as a new column in the elife train dataframe
-df_elife_train_clean['tfidf_summary'] = doc_summaries
 
-# save the summaries to a csv file
-df_elife_train_clean.to_csv('data/elife_summaries.csv', index=True)
+    for doc in doc_list: # For every document
+        no_parens = remove_between_parens(doc) # Remove citations and other parentheses from the document
+        doc_sents = nltk.sent_tokenize(no_parens) # Split the document into sentences
+        doc_length = len(doc_sents) # Get the total number of sentences in the document
+        top_num = int(summary_percent * doc_length) # Calculate the number of sentences to keep for the summary
+        sent_scores = sentence_value_creator_2(doc_sents, vector_scores) # Get list of (index, score) pairs for all the document sentences
+        sorted_scores = sorted(sent_scores, key=lambda x: x[1]) # Sort based on second tuple object; sort by score
+        sorted_scores = sorted_scores[-top_num:] # Crop to just the top top_num sents
+        sorted_sents = sorted(sorted_scores, key = lambda x: x[0]) # Sort the top sents by index so they are in the logical order
+        doc_summary = "" # Save summary of the document as string
+        for (index, score) in sorted_sents: # For every (index, score) pair
+            sent = doc_sents[index] # Get the original sentence using the index
+            doc_summary = doc_summary + sent + " " # Add the original sent to the full summary 
+        doc_summaries.append(doc_summary) # Add the summary to the list of all summaries
 
-# save the tfidf_summary column to a text file
-with open('data/elife_summaries.txt', 'w') as f:
-    for summary in df_elife_train_clean['tfidf_summary']:
-        f.write(summary + '\n')
+    # Save the summaries as a new column in the elife train dataframe
+    df_clean['tfidf_summary'] = doc_summaries
 
-with open('elife_summaries.txt', 'w') as f:
-    for summ in summary_list:
-        f.write(summ.replace('\n','') + '\n')
+    # save the summaries to a csv file
+    df_clean.to_csv(args.output_csv, index=True)
+
+    # save the tfidf_summary column to a text file
+    with open('data/elife_summaries.txt', 'w') as f:
+        for summary in df_clean['tfidf_summary']:
+            f.write(summary + '\n')
+
+    summary_list = df_clean.tfidf_summary.tolist()
+    
+    with open('elife_summaries.txt', 'w') as f:
+        for summ in summary_list:
+            f.write(summ.replace('\n','') + '\n')
