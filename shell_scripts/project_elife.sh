a0d501180fc16626f89f52551d54435ba3c8feec
Jen Wilson <12726758+zepam@users.noreply.github.com>
2025-06-04 09:59:23 -0700

    update files with new directory organization paths

---
+++ shell_scripts/project_elife.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# DATA CLEANING: Creates new dataframes with a "clean" column
+# Run with elife training data, which is the default args
+python python_scripts/data_cleaning.py
+
+# Run with 1/3 Elife training data
+python python_scripts/data_cleaning.py --parquet_path "data/Elife/train-00000-of-00003.parquet" --output_csv "data/df_Elife_train_clean_00000_of_00003.csv"
+
+# Run with 2/3 Elife training data
+python python_scripts/data_cleaning.py --parquet_path "data/Elife/train-00001-of-00003.parquet" --output_csv "data/df_Elife_train_clean_00001_of_00003.csv"
+
+# Run with 3/3 Elife training data
+python python_scripts/data_cleaning.py --parquet_path "data/Elife/train-00002-of-00003.parquet" --output_csv "data/df_Elife_train_clean_00002_of_00003.csv"
+
+#TF-IDF EXTRACTIVE SUMMARIES: Creates new dataframes with a "tfidf_summary" column
+# Run with elife training data, which is the default args
+python python_scripts/tfidf.py
+
+# Run with 1/3 Elife training data
+python python_scripts/tfidf.py --clean_csv "data/df_Elife_train_clean_00000_of_00003.csv" --output_json "data/Elife_train_summaries_00000_of_00003.json" --output_txt "data/Elife_summaries_00000_of_00003.txt"
+
+# Run with 2/3 Elife training data
+python python_scripts/tfidf.py --clean_csv "data/df_Elife_train_clean_00001_of_00003.csv" --output_json "data/Elife_train_summaries_00001_of_00003.json" --output_txt "data/Elife_summaries_00001_of_000003.txt"
+
+# Run with 3/3 Elife training data
+python python_scripts/tfidf.py --clean_csv "data/df_Elife_train_clean_00002_of_00003.csv" --output_json "data/Elife_train_summaries_00002_of_00003.json" --output_txt "data/Elife_summaries_00002_of_000003.txt"
---
+++ shell_scripts/readme.md
@@ -0,0 +1,28 @@
+## This folder contains shell scripts that run the pipeline.
+
+```python
+.
+├── project_elife.sh
+├── project_plos.sh
+├── readme.md
+├── testing_hf_pipeline.sh
+├── testing_pipeline.sh
+└── training_pipeline.sh
+```
+
+## How to run these scripts
+
+project_elife.sh
+`./shell_scripts/project_elife.sh`
+
+project_plos.sh
+`./shell_scripts/project_plos.sh`
+
+testing_hf_pipeline.sh
+`shell_scripts/testing_hf_pipeline.sh your_test_data your_output_file`
+
+testing_pipeline.sh
+`shell_scripts/testing_pipeline.sh your_model_location your_test_file your_output_file`
+
+training_pipeline.sh
+`shell_scripts/training_pipeline.sh your_model_location your_test_file your_output_file`
